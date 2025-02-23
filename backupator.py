0fa4a242ff10101d8e80ab5732bb057f26c0061a
ipserc <ipserc@users.noreply.github.com>
2025-02-23 18:22:18 +0100

    Descriptio update

--- backupator.py
+++ backupator.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # encoding: utf-8
 '''
-backupator makes folders and files copies from origin to destination  
+backupator makes folders and files copies from source directory to destination  
 
 backupator makes folders and files (fobjects from now on) copies from origin to destination.
 It only copies fobjects if they don't exsit. In case the files exist they are updated if the same files have different md5sum 
